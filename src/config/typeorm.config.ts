0446cfa1d49f1e6098cd8dcbdfa0d0758a101762
hsintzuli <hsintzuli719@gmail.com>
2022-06-25 14:01:54 +0800

    Initial Commit

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  entities: [__dirname + '/../entity/*.entity{.ts,.js}'],
+};
